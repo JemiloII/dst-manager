26fcaa13af7d1d0dcedbfd94e886a4826b601787
Brian Jemilo II <jemiloii@gmail.com>
2026-02-23 03:48:13 -0600

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Don't Starve Server Manager
+Just a project to manage multiple DST servers on Linux
+Maybe I'll code it for windows eventually.
---
+++ dst/Master/modoverrides.lua
@@ -0,0 +1,34 @@
+return {
+  ["workshop-1185229307"]={
+    configuration_options={
+      CAMERA=true,
+      CAPTURE=false,
+      DAMAGE_NUMBERS=true,
+      DAMAGE_RESISTANCE=true,
+      FRAME_PHASES=true,
+      GLOBAL=false,
+      GLOBAL_NUMBERS=false,
+      HEADER_CLIENT=false,
+      HEADER_SERVER=false,
+      HORIZONTAL_OFFSET=0,
+      TAG="EPIC",
+      WETNESS_METER=false 
+    },
+    enabled=true 
+  },
+  ["workshop-1207269058"]={ configuration_options={  }, enabled=true },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ["workshop-466732225"]={ configuration_options={  }, enabled=true },
+  ["workshop-812713308"]={ configuration_options={ fireflies_fear=222, language=111 }, enabled=true } 
+}
